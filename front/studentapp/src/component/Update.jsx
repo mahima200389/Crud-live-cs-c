18d30abbabf3c486cf2d339816b657f0cbb5fc49
mahima200389 <mahimagzb20032gmail.com>
2025-05-01 10:12:46 +0530

    Crud-live cs-c

--- front/studentapp/src/component/Update.jsx
+++ front/studentapp/src/component/Update.jsx
@@ -8,7 +8,7 @@ function Update() {
         const name=e.target.name.value;
         const age =e.target.age.value;
         const data={name,age}
-        await axios.put(`http://localhost:9000/users/${id}`,data)
+        await axios.put(`https://crud-live-cs-c-back.onrender.com/users/${id}`,data)
         alert("DATA UPDATE")
     }
   return (
